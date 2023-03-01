e4e5c593d43d9b0c61ee73fbe0203b3cb06a8e80
Gianfranco Duminuco <gianfranco.duminuco@gmail.com>
2023-03-01 18:30:48 +0100

    finalizzazione

--- OBJECT ORIENTATION/src/GUI/VisualizzaConferenza.java
+++ OBJECT ORIENTATION/src/GUI/VisualizzaConferenza.java
@@ -106,9 +106,6 @@ public class VisualizzaConferenza {
 			}
 			
 		){
-			/**
-			 * 
-			 */
 			private static final long serialVersionUID = 1L;
 			boolean[] columnEditables = new boolean[] {
 				false, false, false, false
